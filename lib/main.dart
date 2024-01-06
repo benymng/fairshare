4bc83e5509f565f12e1158221382371bce290243
Benjamin Ng <60500272+benymng@users.noreply.github.com>
2024-01-05 19:49:31 -0700

    example api get call

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,54 @@
-import 'dart:ffi';
+// import 'dart:ffi';
 
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<List<User>> fetchUsers() async {
+  var client = http.Client();
+  try {
+    var uri = Uri.http('127.0.0.1:8000', '/get_users/');
+    final response = await client.get(uri);
+    if (response.statusCode == 200) {
+      debugPrint('movieTitle: ${response.body}');
+      List<dynamic> list = jsonDecode(response.body);
+      return list
+          .map((data) => User.fromJson(data as Map<String, dynamic>))
+          .toList();
+    } else {
+      print("error");
+      throw Exception('Failed to load users');
+    }
+  } catch (e) {
+    print(e);
+
+    throw Exception('Failed to load users: $e');
+  }
+}
+
+class User {
+  final int id;
+  final String email;
+  final String name;
+  final String dateJoined;
+
+  User(
+      {required this.id,
+      required this.email,
+      required this.name,
+      required this.dateJoined});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      email: json['email'],
+      name: json['name'],
+      dateJoined: json['date_joined'],
+    );
+  }
+}
 
 void main() {
   runApp(MyApp());
@@ -15,11 +61,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
-      child: MaterialApp( 
+      child: MaterialApp(
         title: 'Fairshare',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 221, 146, 170)),
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Color.fromARGB(255, 221, 146, 170)),
         ),
         home: MyHomePage(),
       ),
@@ -30,7 +77,7 @@ class MyApp extends StatelessWidget {
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
 
-  void getNext(){
+  void getNext() {
     current = WordPair.random();
     notifyListeners();
   }
@@ -43,15 +90,15 @@ class MyHomePage extends StatelessWidget {
 
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add_a_photo_outlined),
-        onPressed: () {
-          print("presssed");
-      }),
+          child: Icon(Icons.add_a_photo_outlined),
+          onPressed: () {
+            print("presssed");
+          }),
       bottomNavigationBar: BottomNavigationBar(
         items: [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
-            label: "Home",  
+            label: "Home",
           ),
           BottomNavigationBarItem(
             icon: Icon(Icons.settings),
@@ -79,6 +126,30 @@ class MyHomePage extends StatelessWidget {
                 appState.getNext();
               },
               child: Text('Next'),
+            ),
+            FutureBuilder<List<User>>(
+              future: fetchUsers(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text('An error occurred ${snapshot.error}');
+                } else if (snapshot.connectionState ==
+                    ConnectionState.waiting) {
+                  return CircularProgressIndicator();
+                } else {
+                  return Expanded(
+                    child: ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) {
+                        User user = snapshot.data![index];
+                        return ListTile(
+                          title: Text(user.name),
+                          subtitle: Text(user.email),
+                        );
+                      },
+                    ),
+                  );
+                }
+              },
             ),
           ],
         ),
@@ -110,4 +181,3 @@ class TestWidget extends StatelessWidget {
     );
   }
 }
-
